10593c99dc101786e348bdff9cd211d442bca6a8
thescorpiolab <support@thescorpiolab.com>
2019-03-15 21:30:19 +0000

    March 15 2019 14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,24 @@ class PlacesController < ApplicationController
 	def index
 		@places = Place.all
 	end
+
+	def index
+ 		 @places = Place.order("name").page(params[:page]).per_page(1)
+	end
+
+	def new
+		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end	
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end	
+
 end
