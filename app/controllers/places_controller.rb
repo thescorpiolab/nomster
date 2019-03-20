0f783b4adea432d992e2f055a102c96ca84f8773
thescorpiolab <support@thescorpiolab.com>
2019-03-20 20:06:53 +0000

    March 20 2019 25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 	def index
 		@places = Place.all
@@ -14,9 +14,9 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
-		redirect_to root_path
-	end	
+        Place.create(place_params)
+        redirect_to root_path
+    end
 
 	def show
 		@place = Place.find(params[:id])
@@ -24,10 +24,17 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+    		return render plain: 'Not Allowed', status: :forbidden
+  		end
 	end	
 
 	def update
   		@place = Place.find(params[:id])
+  		if @place.user != current_user
+  			return render plain: 'Not Allowed', status: :forbidden
+  		end	
   		@place.update_attributes(place_params)
   		redirect_to root_path
 	end
@@ -42,11 +49,6 @@ class PlacesController < ApplicationController
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
-	end	
-
-	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
 	end	
 
 end
